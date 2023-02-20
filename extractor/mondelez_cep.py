81f22d29ae22c358b0ebe71ef7aad9cc5ff4363f
Praveen-Kumar-SGK <109057699+Praveen-Kumar-SGK@users.noreply.github.com>
2023-02-20 15:01:53 +0530

    Nutrition Table Added

--- extractor/mondelez_cep.py
+++ extractor/mondelez_cep.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass, field
 # Loading model
 classifier = joblib.load(mondelez_cep_model_loc)
 # ---------------------------------------------------------------------------------------------------------------------
-
+nutri_table_available = False
 @dataclass
 class Mondelez_CEP_Template():
     splt_parameter: str = r"\.\r|\. |\.\t|\:\s|\.b\$1"
@@ -29,16 +29,35 @@ class Mondelez_CEP_Template():
         return text.strip()
 
     # -----------------------------------------------------------------------------------------------------------------
+    def is_nutrition_table_available(self, text):
+
+        nutri_header = ['nutritional value','კვებითი ღირებულება','nutrition value','nutrition']
+        text = re.sub(r'\d','',text)
+        text = re.sub(r'\b\D{1}\b', '', text)
+        similarity = cosine_similarity(laser.embed_sentences(text, lang='en'),
+                                       laser.embed_sentences(nutri_header, lang='en').mean(0).reshape(1, 1024))[0][0]
+        # print("**********",text,"&&&&&&&&&&&&",similarity)
+        if similarity > 0.8:
+            return True
+        else:
+            return False
+    # -----------------------------------------------------------------------------------------------------------------
     def dict_to_list(self, dictionary):
         final_list = []
+        global nutri_table_available
         for key, value in dictionary.items():
-            #print(value)
             for data_dict in value:
-                for text_frame_no,txt in data_dict.items():
-                    item = re.sub(str(self.splt_parameter), lambda pat: pat.group()+"*#", txt, flags=re.M)
+                for text_frame_no, txt in data_dict.items():
+                    item = re.sub(str(self.splt_parameter), lambda pat: pat.group() + "*#", txt, flags=re.M) ## For applying into multi line content
                     item = str(item).split("*#")
-                    for k in item:
+                    itms = []
+                    for itm in item:
+                        itms.extend(str(itm).split("\r"))
+
+                    for k in itms:
                         if k.strip():
+                            if self.is_nutrition_table_available(self.text_preprocessing(k.split("/")[0])):
+                                nutri_table_available = True
                             final_list.append(k.strip())
         return final_list
 
@@ -144,11 +163,130 @@ class Mondelez_CEP_Template():
         return gen_cate_dic
 
     # -----------------------------------------------------------------------------------------------------------------
+    def nutrition_data_processing(self, input_list, method=None):
+        # print(input_list)
+        nutrition_dict = {}
+        nutrition_dict_exception = {}
+        for nutrition_row_dict in input_list:
+            nutrition_header_original = nutrition_row_dict["1"]
+            # nutrition_header = nutrition_row_dict["1"].split('/')[-1].strip()
+            nutrition_header = str(sorted(nutrition_row_dict["1"].split("/"), key=len, reverse=True)[0]).strip()
+            if nutrition_header and len(nutrition_row_dict) > 1:
+                if method == "manual":
+                    nutri_probability = 1
+                    nutri_class = nutrition_header
+                else:
+                    nutrition_header = re.sub(r"(g|mg|kj|kcal|mcg|\/)*\b", "", nutrition_header.lower())
+                    nutri_out = base('ferrero_header', master_nutrition_model_location).prediction(get_display(nutrition_header))
+                    nutri_class = nutri_out['output']
+                    nutri_probability = nutri_out['probability']
+                    print(nutrition_header, '------>', nutri_class)
+                nutrition_row_dict.pop('1', None)
+                if nutrition_row_dict:
+                    if nutri_class not in ['None', 'header', 'Nutrition information'] and nutri_probability > 0.60:
+                        for index, value in nutrition_row_dict.items():
+                            header = 'PDV' if "%" in value else 'Value'
+                            value = value.strip()
+                            if not value:  # textract null value and value with header fix
+                                for regex_value in re.finditer(
+                                        r"\d{0,}?\,?\d{0,}?\s{0,1}?(g|mg|kj|kcal|mcg)\/?(g|mg|kj|kcal|mcg)?",
+                                        nutrition_header_original, re.I):
+                                    if re.search(r"\d", str(regex_value.group())):
+                                        value = (regex_value.group()).strip()
+                                        break
+                            if value.strip():
+                                if nutri_class in nutrition_dict:
+                                    nutrition_dict[nutri_class].append({header: {'en': value}})
+                                else:
+                                    nutrition_dict[nutri_class] = [{header: {'en': value}}]
+                    elif nutri_class not in ['header', 'Nutrition information'] and len(nutrition_header) > 2:
+                        # print("else statement ra")
+                        nutrition_header = re.sub(r"(g|mg|kj|kcal|mcg|\/)*\b", "", nutrition_header.lower())
+                        if nutrition_header.lower() not in ("vitamine") and not re.search(r"\d", nutrition_header):
+                            # if nutrition_header.lower() not in ("vitamine"):
+                            nutri_class = nutrition_header
+                        else:
+                            continue
+                        for index, value in nutrition_row_dict.items():
+                            header = 'PDV' if "%" in value else 'Value'
+                            value = value.strip()
+                            if not value:  # textract null value and value with header fix
+                                for regex_value in re.finditer(
+                                        r"\d{0,}?\,?\d{0,}?\s{0,1}?(g|mg|kj|kcal|mcg)\/?(g|mg|kj|kcal|mcg)?",
+                                        nutrition_header_original, re.I):
+                                    if re.search(r"\d", str(regex_value.group())):
+                                        value = (regex_value.group()).strip()
+                                        break
+                            if value.strip():
+                                if nutri_class in nutrition_dict_exception:
+                                    nutrition_dict_exception[nutri_class].append({header: {'en': value}})
+                                else:
+                                    nutrition_dict_exception[nutri_class] = [{header: {'en': value}}]
+        # print(nutrition_dict)
+
+        if len(nutrition_dict) <= 3:
+            nutrition_dict.clear()
+        else:
+            nutrition_dict = {**nutrition_dict, **nutrition_dict_exception}
+        return nutrition_dict
+
+    # -----------------------------------------------------------------------------------------------------------------
     def mondelez_cep_main(self, dic):
+        output_dic = {}
+        nutrition_aws_mode = 0
+        nutrition_manual_mode = 0
+        nutrition_availability = 0
+        global nutri_table_available
+        nutri_table_available = False
         if "modifyData" in dic:
             return dic["modifyData"]
+        if "nutrition_data" in dic:
+            if "tf_nutrition" in dic["nutrition_data"][0]:
+                nutrition_availability = 1
+                if dic["nutrition_data"][0]['tf_nutrition']:
+                    if isinstance(dic["nutrition_data"][0]['tf_nutrition'][0], str):
+                        nutrition_aws_mode = 1
+                    try:
+                        key_variable = list(dic["nutrition_data"][0]['tf_nutrition'][0].keys())[0]
+
+                        # print("key_variable--------->", key_variable)
+                        if not key_variable.isnumeric():
+                            # print('manual format')
+                            nutrition_manual_mode = 1
+                            nutrition_aws_mode = 0
+                            xx = []
+                            for index, dictionary in enumerate(dic["nutrition_data"][0]['tf_nutrition']):
+                                d = {}
+                                for key, value in dictionary.items():
+                                    d['1'] = key
+                                    for ind, val in enumerate(value):
+                                        d[ind + 2] = val
+                                xx.append(d)
+                            # print(f'xxxx----->{xx}')
+                            nutrition_response = self.nutrition_data_processing(xx, method='manual')
+                        else:
+                            # print('semi-textract format')
+                            nutrition_response = self.nutrition_data_processing(
+                                dic["nutrition_data"][0]['tf_nutrition'])
+                            nutrition_aws_mode = 1
+                    except:
+                        nutrition_response = {}
+                        # return {'status': '0','nutrition':nutrition_response}
+                    if nutrition_response and 'Nutrition' not in output_dic:
+                        output_dic['Nutrition'] = nutrition_response
+        dic.pop('nutrition_data', None)
         txt_list = self.dict_to_list(dic)
-        output_dic = self.final_dict(txt_list, classifier)
-        return {**{'status': '0'}, **{"modifyData": output_dic}}
+        output_dic = {**self.final_dict(txt_list, classifier), **output_dic}
+        # print(output_dic)
+        if nutrition_aws_mode == 1 or not nutri_table_available or nutrition_availability:
+            return {**{'status': '0'}, **{
+                "modifyData": output_dic}}  # Status "0" goes to CEP for edit option else go to tornado for xml generation
+        elif nutrition_manual_mode == 1:
+            return output_dic
+        # return {**{'status':'0'}, **{"modifyData":output_dic}}
+        # if not nutri_table_available:
+        #     return {**{'status':'0'}, **{"modifyData":output_dic}}
+        else:
+            return {'status': '0'}
 
 # ---------------------------------------------------------------------------------------------------------------------
