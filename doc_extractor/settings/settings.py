2fec3c02ef5d076ce953786a9328671dd731d058
vijaykanagaraj <Amma@867777>
2023-06-07 19:27:53 +0530

    Adding garbage collector

--- doc_extractor/settings/settings.py
+++ doc_extractor/settings/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # custom garbagecollector middleware
+    'doc_extractor.GarbageCollectorMiddleware.GarbageCollectorMiddleware'
 ]
 
 ROOT_URLCONF = 'doc_extractor.urls'
